811f86b50aed90b152480c300221a77c51130d1a
Ashutosh12-pixel <routrayashutosh8@gmail.com>
2021-02-16 22:28:49 +0530

    first commit

--- collectdata.py
+++ collectdata.py
@@ -16,11 +16,47 @@ classifier=cv2.CascadeClassifier(face_data)
 
 url="http://25.162.21.129:8080/shot.jpg"
 
-image_from_url=urllib.request.urlopen(url)
+data=[]
 
-frame=np.array(bytearray(image_from_url.read()),np.uint8)
+while len(data)<100:
+    image_from_url=urllib.request.urlopen(url)
+    
+    frame=np.array(bytearray(image_from_url.read()),np.uint8)
+    
+    frame=cv2.imdecode(frame,-1)
+    
+    faces=classifier.detectMultiScale(frame,1.5,5)
+    
+    for x,y,w,h in faces:
+        face_frame=frame[y:y+h,x:x+w]
+        cv2.imshow("only face",face_frame)
+        
+        if len(data)<100:
+            print(len(data),"/100")
+            data.append(face_frame)
+        else:
+            break
+    cv2.imshow("capture",frame)
+    if cv2.waitKey(32)==ord("q"):
+        break
+cv2.destroyAllWindows()
 
-frame=cv2.imdecode(frame,-1)
-
-faces=classifier.detectMultiScale(frame)
-
+if len(data)==100:
+    name=input("Enter Fcae Doner Name : \n")
+    for i in range(0,100):
+        cv2.imwrite("image/"+name+"_"+str(i+1)+".jpg",data[i])
+    else:
+        print("completed")
+else:
+    print("Need More Data")
+    
+    
+    
+    
+    
+    
+    
+        
+        
+    
+    
